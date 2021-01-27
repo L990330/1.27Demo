4d78bf10136085264aeccdb201bda270c759ca96
L990330 <77625114+L990330@users.noreply.github.com>
2021-01-27 13:05:26 +0800

    1.27Demo

---
+++ Assets/Scripts/Reverse.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reverse : MonoBehaviour
+{
+    public InputField input;
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(() =>
+        {
+            if (input.text != "")
+            {
+                Rever(input.text);
+            }
+        });
+    }
+    string Rever(string str)
+    {
+        string arr = str;
+        string newstr = string.Empty;
+        while (str.Contains(",") || str.Contains("."))
+        {
+            int d = str.IndexOf(',');
+            int j = str.IndexOf('.');
+            int a = d < j ? d : j;
+            if (d == -1)
+            {
+                a = j;
+            }
+            if (j == -1)
+            {
+                a = d;
+            }
+            for (int i = a - 1; i >= 0; i--)
+            {
+                newstr += str[i];
+            }
+            newstr += str[a];
+            str = str.Remove(0, a + 1);
+        }
+        print("输入：" + arr + "输出：" + newstr);
+        input.text = newstr;
+        return newstr;
+    }
+}
---
+++ Assets/Scripts/吃豆豆/CreateDia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDia : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-4, 4), 0.25f, Random.Range(-4, 4));
+            Dia dia = Instantiate(Resources.Load<Dia>("吃豆豆/Dia"), pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
